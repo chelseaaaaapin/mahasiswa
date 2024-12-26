3361afde63abf0d829319e6508702867c71d021b
chelseaaaaapin <chelseariyantashila06@gmail.com>
2024-12-26 08:02:29 +0700

    Add files via upload

---
+++ mahasiswa.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mahasiswa;
+
+/**
+ *
+ * @author Chealsea
+ */
+public class mahasiswa {
+    void service(){
+        System.out.println("MAHASISWA SEDANG MENSERVICE PC DI LAB");
+    }
+    void belajar(){
+        System.out.println("MAHASISWA SEDANG BELAJAR CODING");
+    }
+    void modifikasi(){
+        System.out.println("MAHASISWA SEDANG MEMODIFIKASI CODINGNYA");
+    }
+    
+}
---
+++ mahasiswaBeraksi.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mahasiswa;
+
+/**
+ *
+ * @author Chealsea
+ */
+public class mahasiswaBeraksi {
+    public static void main(String[] args) {
+    mahasiswa ms = new mahasiswa();
+    ms.service();
+    ms.belajar();
+    ms.modifikasi();
+    
+    }
+    
+}
+            
+    
+
